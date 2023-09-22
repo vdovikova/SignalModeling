acf2044a21e6295fbb99b629df9adfedb3846563
space pirate <vdovikova.elena@yandex.ru>
2023-09-23 00:29:51 +0300

    added harmonic oscillations

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import math
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+timeArray = []
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def graphHarmonic(f, tarr):
+    fd = 1000
+    omega = 2 * math.pi * f / fd
+    A = 1
+    xArray = []
+    for t in range(0, 1000):
+        x = A * math.sin(omega * t)
+        xArray.append(x)
+        tarr.append(t)
+    return xArray
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+fig, axs = plt.subplots(4)
+fig.suptitle('Harmonic signals')
+
+axs[0].plot(timeArray, graphHarmonic(1, timeArray), color='green')
+axs[0].set_title('1 Hz')
+timeArray = []
+axs[1].plot(timeArray, graphHarmonic(2, timeArray), color='blue')
+axs[1].set_title('2 Hz')
+timeArray = []
+axs[2].plot(timeArray, graphHarmonic(4, timeArray), color='red')
+axs[2].set_title('4 Hz')
+timeArray = []
+axs[3].plot(timeArray, graphHarmonic(8, timeArray), color='black')
+axs[3].set_title('8 Hz')
+plt.show()
