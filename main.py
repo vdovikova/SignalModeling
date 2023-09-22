550fe47d4beb40b6cc7c0ffa3ceef63ffe8e3674
space pirate <vdovikova.elena@yandex.ru>
2023-09-23 01:04:24 +0300

    added spectrum for harmonic oscillatoins

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import math
 import matplotlib.pyplot as plt
+from numpy.fft import rfft, rfftfreq
+from numpy import array, arange, abs as np_abs
 
 timeArray = []
 
 
 def graphHarmonic(f, tarr):
-    fd = 1000
+    fd = 1000  # частота дискретизации
     omega = 2 * math.pi * f / fd
     A = 1
     xArray = []
@@ -16,18 +18,38 @@ def graphHarmonic(f, tarr):
     return xArray
 
 
-fig, axs = plt.subplots(4)
+fig, axs = plt.subplots(4, 2)
 fig.suptitle('Harmonic signals')
+fig.tight_layout(h_pad=2)
 
-axs[0].plot(timeArray, graphHarmonic(1, timeArray), color='green')
-axs[0].set_title('1 Hz')
+func = graphHarmonic(1, timeArray)
+axs[0, 0].plot(timeArray, func, color='green')
+axs[0, 0].set_title('1 Hz', fontsize=8)
+spectr_sin = rfft(func)
+axs[0, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[0, 1].set_title('1 Hz, spectrum', fontsize=8)
 timeArray = []
-axs[1].plot(timeArray, graphHarmonic(2, timeArray), color='blue')
-axs[1].set_title('2 Hz')
+
+func = graphHarmonic(2, timeArray)
+axs[1, 0].plot(timeArray, func, color='blue')
+axs[1, 0].set_title('2 Hz', fontsize=8)
+spectr_sin = rfft(func)
+axs[1, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[1, 1].set_title('2 Hz, spectrum', fontsize=8)
 timeArray = []
-axs[2].plot(timeArray, graphHarmonic(4, timeArray), color='red')
-axs[2].set_title('4 Hz')
+
+func = graphHarmonic(4, timeArray)
+axs[2, 0].plot(timeArray, func, color='red')
+axs[2, 0].set_title('4 Hz', fontsize=8)
+spectr_sin = rfft(func)
+axs[2, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[2, 1].set_title('4 Hz, spectrum', fontsize=8)
 timeArray = []
-axs[3].plot(timeArray, graphHarmonic(8, timeArray), color='black')
-axs[3].set_title('8 Hz')
+
+func = graphHarmonic(8, timeArray)
+axs[3, 0].plot(timeArray, func, color='black')
+axs[3, 0].set_title('8 Hz', fontsize=8)
+spectr_sin = rfft(func)
+axs[3, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[3, 1].set_title('8 Hz, spectrum', fontsize=8)
 plt.show()
