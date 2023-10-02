4d89d16c7cab956d65dd8592de1591a837d0df67
space pirate <vdovikova.elena@yandex.ru>
2023-10-02 09:16:20 +0300

    graph's label added and size changed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import math
 import matplotlib.pyplot as plt
 from numpy.fft import rfft, rfftfreq
-from numpy import array, arange, abs as np_abs
+from numpy import heaviside as hvsd
+from numpy import abs as np_abs
 
 timeArray = []
-
 
 def graphHarmonic(f, tarr):
     fd = 1000  # частота дискретизации
@@ -17,57 +17,74 @@ def graphHarmonic(f, tarr):
         tarr.append(t)
     return xArray
 
-def graphDigital (f1, timeArray):
+def graphDigital (f, timeArray):
     fd = 1000
-    w1 = 2 * math.pi * f1 / fd
-    a1 = 4 / math.pi
+    w = 2 * math.pi * f / fd
     xArray = []
     for t in range(0, 1000):
-        x1 = math.sin(w1 * t)  # 1-я гармоника
-        x3 = 1 / 3 * math.sin(3 * w1 * t)  # 3-я гармоника
-        x5 = 1 / 5 * math.sin(5 * w1 * t)  # 5-я гармоника
-        x7 = 1 / 7 * math.sin(7 * w1 * t)  # 7-я гармоника
-        x9 = 1 / 9 * math.sin(9 * w1 * t)  # 9-я гармоника
-        x_sum = a1 * (x1 + x3 + x5 + x7 + x9)  # сумма гармоник
-        xArray.append(x_sum)
+        x = hvsd(math.sin(w * t), 1)
+        xArray.append(x)
         timeArray.append(t)
     return xArray
 
+def digitalSpectrum (f):
+    spectr_sin = rfft(f)
+    farr = []
+    aarr = []
+    barr = []
+    a = rfftfreq(1000, 1. / 1000)
+    b = np_abs(spectr_sin) / 1000
+    if a==0:
+        b = 1
+    barr.append(b)
+    aarr.append(a)
 
 fig, axs = plt.subplots(4, 2)
 fig.suptitle('Harmonic signals')
 fig.tight_layout(h_pad=2)
 
 func = graphHarmonic(1, timeArray)
-axs[0, 0].plot(timeArray, func, color='green')
+axs[0, 0].plot(timeArray, func, color='g')
 axs[0, 0].set_title('1 Hz', fontsize=8)
 spectr_sin = rfft(func)
 axs[0, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[0, 0].set_xlabel("ms", fontsize=7, loc='left')
+axs[0, 1].set_xlim([0, 10])
 axs[0, 1].set_title('1 Hz, spectrum', fontsize=8)
+axs[0, 1].set_xlabel("Hz", fontsize=7, loc='left')
 timeArray = []
 
 func = graphHarmonic(2, timeArray)
 axs[1, 0].plot(timeArray, func, color='blue')
 axs[1, 0].set_title('2 Hz', fontsize=8)
+axs[1, 0].set_xlabel("ms", fontsize=7, loc='left')
 spectr_sin = rfft(func)
 axs[1, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[1, 1].set_xlim([0, 10])
 axs[1, 1].set_title('2 Hz, spectrum', fontsize=8)
+axs[1, 1].set_xlabel("Hz", fontsize=7, loc='left')
 timeArray = []
 
 func = graphHarmonic(4, timeArray)
-axs[2, 0].plot(timeArray, func, color='red')
+axs[2, 0].plot(timeArray, func, color='m')
 axs[2, 0].set_title('4 Hz', fontsize=8)
+axs[2, 0].set_xlabel("ms", fontsize=7, loc='left')
 spectr_sin = rfft(func)
 axs[2, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[2, 1].set_xlim([0, 10])
 axs[2, 1].set_title('4 Hz, spectrum', fontsize=8)
+axs[2, 1].set_xlabel("Hz", fontsize=7, loc='left')
 timeArray = []
 
 func = graphHarmonic(8, timeArray)
-axs[3, 0].plot(timeArray, func, color='black')
+axs[3, 0].plot(timeArray, func, color='r')
 axs[3, 0].set_title('8 Hz', fontsize=8)
+axs[3, 0].set_xlabel("ms", fontsize=7, loc='left')
 spectr_sin = rfft(func)
 axs[3, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[3, 1].set_xlim([0, 10])
 axs[3, 1].set_title('8 Hz, spectrum', fontsize=8)
+axs[3, 1].set_xlabel("Hz", fontsize=7, loc='left')
 plt.show()
 timeArray = []
 
@@ -79,31 +96,43 @@ fig.tight_layout(h_pad=2)
 func = graphDigital(1, timeArray)
 axs[0, 0].plot(timeArray, func, color='green')
 axs[0, 0].set_title('1 Hz', fontsize=8)
+axs[0, 0].set_xlabel("ms", fontsize=7, loc='left')
 spectr_sin = rfft(func)
 axs[0, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[0, 1].set_xlim([0, 50])
 axs[0, 1].set_title('1 Hz, spectrum', fontsize=8)
+axs[0, 1].set_xlabel("Hz", fontsize=7, loc='left')
 timeArray = []
 
 func = graphDigital(2, timeArray)
-axs[1, 0].plot(timeArray, func, color='blue')
+axs[1, 0].plot(timeArray, func, color='b')
 axs[1, 0].set_title('2 Hz', fontsize=8)
+axs[1, 0].set_xlabel("ms", fontsize=7, loc='left')
 spectr_sin = rfft(func)
 axs[1, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[1, 1].set_xlim([0, 50])
 axs[1, 1].set_title('2 Hz, spectrum', fontsize=8)
+axs[1, 1].set_xlabel("Hz", fontsize=7, loc='left')
 timeArray = []
 
 func = graphDigital(4, timeArray)
-axs[2, 0].plot(timeArray, func, color='red')
+axs[2, 0].plot(timeArray, func, color='m')
 axs[2, 0].set_title('4 Hz', fontsize=8)
+axs[2, 0].set_xlabel("ms", fontsize=7, loc='left')
 spectr_sin = rfft(func)
 axs[2, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[2, 1].set_xlim([0, 50])
 axs[2, 1].set_title('4 Hz, spectrum', fontsize=8)
+axs[2, 1].set_xlabel("Hz", fontsize=7, loc='left')
 timeArray = []
 
 func = graphDigital(8, timeArray)
-axs[3, 0].plot(timeArray, func, color='black')
+axs[3, 0].plot(timeArray, func, color='r')
 axs[3, 0].set_title('8 Hz', fontsize=8)
+axs[3, 0].set_xlabel("ms", fontsize=7, loc='left')
 spectr_sin = rfft(func)
 axs[3, 1].plot(rfftfreq(1000, 1./1000), np_abs(spectr_sin)/1000)
+axs[3, 1].set_xlim([0, 50])
 axs[3, 1].set_title('8 Hz, spectrum', fontsize=8)
+axs[3, 1].set_xlabel("Hz", fontsize=7, loc='left')
 plt.show()
